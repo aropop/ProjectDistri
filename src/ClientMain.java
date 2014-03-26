5efe5927f69f1de0bab04367a6cd0b3c8b112853
Arno De Witte <Arno.De.Witte@vub.ac.be>
2014-03-26 21:58:33 +0100

    Initial commit

---
+++ src/ClientMain.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class ClientMain {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String input;
+		System.out.print("> ");
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextLine();
+		while (!input.equals("exit")){
+			//command loop
+			System.out.print("> ");
+			
+			
+			
+			//next command
+			input = sc.nextLine();
+		}
+
+	}
+
+}
